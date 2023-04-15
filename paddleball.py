9eb04f294e482e0eb96973847f2f233549a258b7
anton.holub <anton.holub91@gmail.com>
2023-04-15 10:20:43 +0300

    added possibility to move the paddle and left and right borders for paddle moving

--- paddleball.py
+++ paddleball.py
@@ -31,10 +31,24 @@ class Paddle:
         self.canvas = canvas
         self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
         self.canvas.move(self.id, 200, 300)
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
 
     def draw(self):
-        pass
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords(self.id)
+        if pos[0] <= 0:
+            self.x = 0
+        elif pos[2] >= self.canvas_width:
+            self.x = 0
 
+    def turn_left(self, evt):
+        self.x = -2
+
+    def turn_right(self, evt):
+        self.x = 2
 
 tk = Tk()
 # set name of the game window
@@ -49,10 +63,12 @@ canvas.pack()
 tk.update()
 
 ball = Ball(canvas, "red")
+paddle = Paddle(canvas, "blue")
 
 # Main loop - infinite loop in order not to close game window
 while 1:
     ball.draw()
+    paddle.draw()
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
