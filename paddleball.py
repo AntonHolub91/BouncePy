91dec9e7fdafb46880ba0c7788489c4aa7fb5860
anton.holub <anton.holub91@gmail.com>
2023-04-15 10:47:18 +0300

    added hit_paddle method

--- paddleball.py
+++ paddleball.py
@@ -3,8 +3,9 @@ import random
 import time
 
 class Ball:
-    def __init__(self, canvas, color):
+    def __init__(self, canvas, paddle, color):
         self.canvas = canvas
+        self.paddle = paddle
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
         starts = [-3, -2, -1, 1, 2, 3]
@@ -14,6 +15,13 @@ class Ball:
         self.canvas_height = self.canvas.winfo_height()
         self.canvas_width = self.canvas.winfo_width()
 
+    def hit_paddle(self, pos):
+        paddle_pos = self.canvas.coords(self.paddle.id)
+        if pos[2] >= paddle_pos[0] and pos[0] <=paddle_pos[2]:
+            if pos[3] >= paddle_pos[1] and pos[3] <=paddle_pos[3]:
+                return True
+        return False
+
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
         pos = self.canvas.coords(self.id)
@@ -21,6 +29,8 @@ class Ball:
             self.y = 2
         if pos[3] >= self.canvas_height:
             self.y = -2
+        if self.hit_paddle(pos) == True:
+            self.y = -3
         if pos[0] <= 0:
             self.x = 3
         if pos[2] >= self.canvas_width:
@@ -62,8 +72,8 @@ canvas = Canvas(tk, width=500, height=500, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
 
-ball = Ball(canvas, "red")
 paddle = Paddle(canvas, "blue")
+ball = Ball(canvas, paddle, "red")
 
 # Main loop - infinite loop in order not to close game window
 while 1:
